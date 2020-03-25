d0e68b1a3d3772ea597e0e37a0e03348340faff8
kupuma-ru21 <kupumaru21@gmail.com>
2020-03-25 23:19:25 +0900

    feat: CreateNewBoardModalコンポネートを作成

---
+++ src/components/CreateNewBoardModal.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const CreateNewBoardModal = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <p>What shall we call the board?</p>
+      <Input
+        style={{
+          backgroundColor: '#181a1b',
+          width: 235,
+          borderRadius: 5.5,
+          height: 45,
+        }}
+      />
+      <ButtonGroup size="large" style={{ margin: 20 }}>
+        <Button>CANCEL</Button>
+        <Button style={{ backgroundColor: '#181a1b' }}>CREATE</Button>
+      </ButtonGroup>
+    </div>
+  );
+};
+
+export default CreateNewBoardModal;
