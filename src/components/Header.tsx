0ca5d508dc67478ddcf545d6fdbe6906438c4762
kupuma-ru21 <kupumaru21@gmail.com>
2020-03-26 23:15:55 +0900

    feat: sassを導入(materialUIには使えないらしい・・・)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import HeaderLogo from 'assets/logo.svg';
+import 'styles/Header.scss';
 
 const Header: React.FC = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <img
-        src={HeaderLogo}
-        className="HeaderLogo"
-        alt="HeaderLogo"
-        style={{ height: '60px', width: '60px' }}
-      />
+    <div className="HeaderLogoArea">
+      <img src={HeaderLogo} className="HeaderLogo" alt="HeaderLogo" />
     </div>
   );
 };
