f0f6fb2704aa1b8a5c9e14a526258299d38a42d1
kupuma-ru21 <kupumaru21@gmail.com>
2020-03-26 23:26:27 +0900

    feat: onChangeイベントを追加 event.target.valueの型定義

--- src/components/CreateNewBoardModal.tsx
+++ src/components/CreateNewBoardModal.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
+interface InputWord {
+  target: { value: string };
+}
+
 const CreateNewBoardModal: React.FC = () => {
+  const [inputWord, setInputWord] = useState<string>('');
+  const onChange = (event: InputWord) => {
+    setInputWord(event.target.value);
+  };
   return (
     <div style={{ textAlign: 'center' }}>
       What shall we call the board?
@@ -14,6 +22,7 @@ const CreateNewBoardModal: React.FC = () => {
           borderRadius: 5.5,
           height: 45,
         }}
+        onChange={onChange}
       />
       <ButtonGroup size="large" style={{ margin: 20 }}>
         <Button>CANCEL</Button>
