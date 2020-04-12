f0c125fa4ec96574c62dd8b9fa76ba04696a2d9f
kupuma-ru21 <kupumaru21@gmail.com>
2020-04-12 11:51:25 +0900

    feat: styleをsassで管理するよう修正

--- src/components/SimpleModal.tsx
+++ src/components/SimpleModal.tsx
@@ -1,41 +1,9 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import CancelIcon from '@material-ui/icons/Cancel';
 import AppContext from 'contexts/AppContext';
-import { CLEATE_ADD_LIST, AFER_CLONE_ADD_LIST } from 'actions/index';
+import { AFER_CLONE_ADD_LIST } from 'actions/index';
+import 'styles/SimpleModal.scss';
 
-const AddListAreaStyle = {
-  display: 'flex',
-  backgroundColor: '#0a2c74',
-  margin: 20,
-  padding: '40px 40px',
-  borderRadius: 5,
-  width: 'fit-content',
-  cursor: 'pointer',
-};
-
-const modalStyle = {
-  backgroundColor: '#575757',
-  padding: '20px 12px',
-  width: 250,
-  margin: '20px 0',
-  height: 100,
-  borderRadius: 5,
-  marginLeft: 20,
-};
-
-const inputStyle = {
-  border: '2.5px solid',
-  borderRadius: 5,
-  fontSize: 16,
-  padding: '11px 15px',
-};
-const afterCloneAddListStyle = {
-  backgroundColor: '#575757',
-  display: 'inline-block',
-  verticalAlign: 'top',
-  padding: '5px 10px',
-  margin: 20,
-};
 interface InputWord {
   target: { value: string };
 }
@@ -58,14 +26,11 @@ const SimpleModal = () => {
       setInputWord('');
     }
   };
-  useEffect(() => {
-    dispatch({ type: CLEATE_ADD_LIST });
-  }, [dispatch]);
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       {state.createBoardReducer.afterCloneAddList.map(
         (val: { addListText: string }, index: number) => (
-          <div key={index} style={afterCloneAddListStyle}>
+          <div key={index} className="afterCloneAddListStyle">
             <h4
               style={{
                 textAlign: 'center',
@@ -75,65 +40,35 @@ const SimpleModal = () => {
             </h4>
             <hr />
             <label>
-              <input
-                style={{
-                  padding: '11px 15px',
-                  fontSize: 16,
-                  borderRadius: 5,
-                  width: 200,
-                }}
-              ></input>
+              <input className="afterCloneAddListStyle-input"></input>
             </label>
           </div>
         )
       )}
-      {state.createBoardReducer.cloneAddList.map((val: any, index: number) => (
-        <div key={index}>
-          <div
-            style={
-              addListFlag === true ? AddListAreaStyle : { display: 'none' }
-            }
-            onClick={onClick}
-          >
-            {val.addListText}
-          </div>
-          <div style={addListFlag === false ? modalStyle : { display: 'none' }}>
-            <div>
-              <form style={{ position: 'relative', marginBottom: 20 }}>
-                <input
-                  style={inputStyle}
-                  placeholder="add a list"
-                  value={inputWord}
-                  onChange={addListInput}
-                />
-                <CancelIcon
-                  style={{
-                    position: 'absolute',
-                    right: '-20px',
-                    top: '-25px',
-                    cursor: 'pointer',
-                  }}
-                  onClick={onClick}
-                />
-              </form>
-              <div
-                style={{
-                  textAlign: 'center',
-                  backgroundColor: '#9fe7a4',
-                  width: 80,
-                  padding: '10px 0',
-                  borderRadius: 5,
-                  float: 'right',
-                  cursor: 'pointer',
-                }}
-                onClick={onSave}
-              >
-                {val.saveText}
-              </div>
+      <div>
+        <div
+          className={addListFlag === true ? 'AddListAreaStyle' : 'displayNone'}
+          onClick={onClick}
+        >
+          Add a list...
+        </div>
+        <div className={addListFlag === false ? 'modalStyle' : 'displayNone'}>
+          <div>
+            <form style={{ position: 'relative', marginBottom: 20 }}>
+              <input
+                className="inputStyle"
+                placeholder="add a list"
+                value={inputWord}
+                onChange={addListInput}
+              />
+              <CancelIcon className="cancelIcon" onClick={onClick} />
+            </form>
+            <div className="saveButton" onClick={onSave}>
+              保存
             </div>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
